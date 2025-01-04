f56aa36b6a5c4d6bebb5a70800edc256573b7c5e
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-04 18:22:01 +0100

    Update copia_di_animatedstock.py

--- copia_di_animatedstock.py
+++ copia_di_animatedstock.py
@@ -80,7 +80,7 @@ try:
         combined_data = pd.concat([combined_data, hist])
     
 # Riorganizza i dati per costruire il portafoglio
-        pivot_data = combined_data.pivot_table(values='Close', index=combined_data.index, columns='Ticker')
+    pivot_data = combined_data.pivot_table(values='Close', index=combined_data.index, columns='Ticker')
 
 
 
