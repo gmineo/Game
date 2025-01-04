4e5aee50ab3039f641acd234c79afa47db78d1e6
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-04 23:34:43 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -57,9 +57,13 @@ stock_input = st.multiselect("Select stocks:", options=df['name'])
 
 # Process the input string to get a list of stock symbols
 #stock_symbols = [symbol.strip().upper() for symbol in stock_input.split(",")]
-stock_symbols = stock_input['ticker'].tolist()
+
+selected_tickers = df[df['name'].isin(stock_input)]
+selected_stock = selected_stocks['ticker'].tolist()
+
+
 # Create a selection box for choosing which stock to display
-selected_stock = st.selectbox("Select stock to display", stock_symbols)
+#selected_stock = st.selectbox("Select stock to display", stock_symbols)
 
 
 try:
