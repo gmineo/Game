a1cfb0bc414fda6407c894d004776c648798fa40
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-04 23:42:26 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -51,20 +51,9 @@ df = pd.DataFrame(data)
 # Menu a tendina per selezionare più titoli
 stock_input = st.multiselect("Select stocks:", options=df['name'])
 
-
-
-
-
-# Process the input string to get a list of stock symbols
-#stock_symbols = [symbol.strip().upper() for symbol in stock_input.split(",")]
-
 selected_stocks = df[df['name'].isin(stock_input)]
 selected_tickers = selected_stocks['ticker']
 selected_stock = st.selectbox("Select stock to display", selected_tickers)
-
-#orig
-# Create a selection box for choosing which stock to display
-#selected_stock = st.selectbox("Select stock to display", stock_symbols)
 
 
 try:
