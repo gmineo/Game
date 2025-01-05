58258c291ea82eb7c447a85f10c9189d779012f6
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-05 22:47:32 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -64,7 +64,7 @@ try:
     # Fetch stock data for the selected symbol
         stock = yf.Ticker(selected_stock)
         #hist = stock.history(start=f"{max_ipo_year}-01-01") 
-        hist = stock.history(start="1980-01-01") 
+        hist = stock.history(max_ipo_year) 
 
     # Reset index to get 'Date' as a column
     hist.reset_index(inplace=True)
