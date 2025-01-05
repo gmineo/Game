8348ac088a8443fa4bfeb58ba16c0a79dfff3b7e
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-05 23:58:36 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -68,7 +68,7 @@ try:
         #hist = stock.history(start="2019-02-01", interval="1wk") #funzionante, prob non riesce a fre grafici con troppi punti
         #hist = stock.history(period="max",interval="1wk" ) #funzionante
         hist = stock.history(start=f"{max_ipo_year}-01-01",interval="1wk" )
-        #hist['Ticker'] = ticker  # Aggiunge una colonna per identificare il titolo
+        hist['Ticker'] = ticker  # Aggiunge una colonna per identificare il titolo
         combined_data = pd.concat([combined_data, hist])
 
       
