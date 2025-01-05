8c7a231acc014ea6f1163a0c77cf40abfc4adc60
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-05 23:18:56 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -64,7 +64,7 @@ try:
     # Fetch stock data for the selected symbol
         stock = yf.Ticker(selected_stock)
         #hist = stock.history(start="1981-01-01") 
-        hist = stock.history(period="max") 
+        hist = stock.history() 
         
       
 
