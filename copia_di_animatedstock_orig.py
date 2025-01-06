aa5a7a31b8f713f5e515a0b19abdee0e0fe79228
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-06 22:44:30 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -124,8 +124,6 @@ hist = pd.DataFrame(hist)
 st.write("Ecco un'anteprima di hist:")
 st.dataframe(hist)
 
-# Rinomina la colonna della Series convertita (se necessario)
-hist.rename(columns={hist.columns[1]: "Close"}, inplace=True)
 # Reimposta l'indice e aggiungi la colonna "Date"
 hist.reset_index(inplace=True)
 # Rinomina le colonne in "Date" e "Close"
