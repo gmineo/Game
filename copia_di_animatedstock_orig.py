da176767e241e3e27c37d38f47429b49aa52e20a
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-06 22:08:22 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -118,7 +118,7 @@ fig = go.Figure()
 frames = [
         go.Frame(
             data=[
-                go.Scatter(x=hist['Date'][:k+1], y=hist['Close'][:k+1], mode='lines', name='Close Price')
+                go.Scatter(x=hist['Date'][:k+1], y=hist['0'][:k+1], mode='lines', name='Close Price')
             ],
             name=str(k)
         ) for k in range(len(hist))
