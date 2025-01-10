b2ef6649abb7b1fdacbe518bd64afda8b9cc07a3
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-10 22:11:34 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -116,7 +116,12 @@ st.write("Ecco un'anteprima di returns:")
 st.dataframe(returns)
 
 # Calcola i ritorni cumulativi
-cumulative_returns = (1+returns).cumprod()
+#cumulative_returns = (1+returns).cumprod()
+cumulative_returns = returns.groupby('Ticker')['Close'].apply(lambda x: (x / x.iloc[0]) - 1)
+
+
+
+
 
 st.write("Ecco un'anteprima di cumulative_returns:")
 st.write(cumulative_returns)
