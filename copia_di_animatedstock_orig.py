fe6d48fe0b413a5a2b78b67a28c63458c677a507
Gmineo <39634149+gmineo@users.noreply.github.com>
2025-01-10 21:26:44 +0100

    Update copia_di_animatedstock_orig.py

--- copia_di_animatedstock_orig.py
+++ copia_di_animatedstock_orig.py
@@ -116,10 +116,14 @@ st.dataframe(returns)
 
 # Calcola i ritorni cumulativi
 cumulative_returns = (1 + returns).cumprod()
+
+st.write("Ecco un'anteprima di cumulative_returns:")
 st.write(cumulative_returns)
 # Portafoglio equally weighted
 portfolio_weights = [1/num_tickers]
 portfolio_cumulative_returns = (cumulative_returns * portfolio_weights).sum(axis=1)
+
+st.write("Ecco un'anteprima di portfolio_weights:")
 st.write(portfolio_weights)
 
 
